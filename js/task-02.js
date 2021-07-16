0a8aed58c47d464028b7ce3f48e108543be93914
Vitaliy <chichygav@gmail.com>
2021-07-16 22:03:48 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -10,17 +10,34 @@ const ingredients = [
   'Приправы',
 ];
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const ingredient = ingredients[i];
-  console.log(ingredient);
-const ingredientq = document.createElement('li');
-ingredientq.textContent = (ingredient);
-console.log(ingredientq);
-const ingredientEl = document.querySelector(`ul#ingredients`);
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = ingredients[i];
+//   console.log(ingredient);
+// const ingredientq = document.createElement('li');
+// ingredientq.textContent = (ingredient);
+// console.log(ingredientq);
+// const ingredientEl = document.querySelector(`ul#ingredients`);
 
-ingredientEl.appendChild(ingredientq);
- };
+// ingredientEl.appendChild(ingredientq);
+//  };
 
 
 
- 
+ const parentOfList = document.querySelector('#ingredients');
+
+const createListOfIngredients = (list) => {
+  let itemEl;
+  const listOfIngredients = [];
+  for (let item of list) {
+    itemEl = document.createElement('li')    
+    itemEl.textContent = item;    
+    listOfIngredients.push(itemEl);  
+   }
+  
+  return listOfIngredients;
+}
+
+
+console.log(createListOfIngredients(ingredients));
+const listEl = createListOfIngredients(ingredients); 
+parentOfList.append(...listEl);
